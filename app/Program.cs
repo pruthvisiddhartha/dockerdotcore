2cea9e84786368d687e31b955792fb9a4a1016dd
Prudhvi <PrudhviSiddharth@outlook.com>
2019-07-24 03:40:21 -0700

    first dotcore app

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("Enter your name");
+		string _name = Console.ReadLine();
+		Console.WriteLine("Hello {0} to the  World of dockerdotnetcore !", _name);
+        }
+    }
+}
